80256aaef9e85b76f9d2db4ae27f70d814c8cd45
r-rindo <keire1970f@gmail.com>
2022-11-23 08:22:28 +0000

    php/laravel 09

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use App\Http\Controllers\XXX\AAAController;
+Route::controller(AAAController::class)->group(function(){Route::get('XXX/AAAController','bbb');});
+
+
+use App\Http\Controllers\Admin\profileControler;
+Route::controller(profileController::class)->group(function(){Route::get('admin/profile/create','add');});
+Route::controller(profileController::class)->group(function(){Route::get('admin/profile/edit','edit');});
