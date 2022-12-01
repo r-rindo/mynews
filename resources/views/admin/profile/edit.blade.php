6879490d0ba673d79d1dc1c8961270f58a2ad9ef
r-rindo <keire1970f@gmail.com>
2022-12-01 06:01:17 +0000

    php/laravel 12

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible"content="IE=edge">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <title>MyNews</title>
-    </head>
-    <body>
-        <h1>課題4 edit</h1>
-    </body>
-</html>
+@extends('layouts.profile')
+@section('title', 'editファイル')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>editファイル</h2>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,18 @@ Route::get('/', function () {
 });
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+    Route::get('news/create', 'add')->middleware('auth');
 });
 
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->group(function(){Route::get('admin/profile/create','add');});
-Route::controller(ProfileController::class)->group(function(){Route::get('admin/profile/edit','edit');});
+Route::controller(ProfileController::class)->group(function(){
+    Route::get('admin/profile/create','add')->middleware('auth');
+    
+});
+Route::controller(ProfileController::class)->group(function(){
+    Route::get('admin/profile/edit','edit')->middleware('auth');
+    
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
