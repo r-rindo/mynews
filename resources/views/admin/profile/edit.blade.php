80f855cfd202f8b9f5627c911e3cb925087c547b
r-rindo <keire1970f@gmail.com>
2022-12-01 13:01:06 +0000

    php/laravel 13

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -5,6 +5,7 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>editファイル</h2>
+                <form action="{{ route('admin.profile.create') }}" method="post" enctype="multipart/form-data"></form>
             </div>
         </div>
     </div>
